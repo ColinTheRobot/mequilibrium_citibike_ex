da159f9ff14ff17b9f3d28d04dfa2bec7f877525
Colin Hart <me@colin-hart.com>
2017-06-22 17:40:12 -0400

    fix white space and change header tags to mui syntax"

--- public/scripts/directives/station.directive.js
+++ public/scripts/directives/station.directive.js
@@ -9,9 +9,13 @@ function station() {
     restrict: 'E',
     controller: 'HomeController as home',
     template: `
-        <h4>{{data.name}}</h4>
-        <p>Available Bikes: {{data.free_bikes}}</p>
+      <div class="station-name mui--text-dark mui--text-headline">{{data.name}}</div>
+      <div class="mui--text-dark-secondary mui--text-caption">Available Bikes: {{data.free_bikes}}</div>
 
+    <dl>
+      <dd class="percentage percentage-{{home.getPercentage(data.free_bikes, data.empty_slots)}}">
+      </dd>
+    </dl>
         <div>
           <button class="mui-btn mui-btn--small mui-btn--fab mui-btn--danger"type="button" ng-click="home.decrement(data)"> - </button>
           <button class="mui-btn mui-btn--small mui-btn--fab mui-btn--primary" type="button" ng-click="home.increment(data)"> + </button>
