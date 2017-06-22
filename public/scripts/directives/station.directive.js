7b4abd1f04b30bd1f506f8e72472983d02737858
Colin Hart <me@colin-hart.com>
2017-06-22 17:08:52 -0400

    copy change

--- public/scripts/directives/station.directive.js
+++ public/scripts/directives/station.directive.js
@@ -10,7 +10,7 @@ function station() {
     controller: 'HomeController as home',
     template: `
         <h4>{{data.name}}</h4>
-        <p>Free Bikes: {{data.free_bikes}}</p>
+        <p>Available Bikes: {{data.free_bikes}}</p>
 
         <div>
           <button class="mui-btn mui-btn--small mui-btn--fab mui-btn--danger"type="button" ng-click="home.decrement(data)"> - </button>
