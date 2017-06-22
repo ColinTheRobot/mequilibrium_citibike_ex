052207fe08ba8c0d911a6ecfe02c81a44674e61d
Colin Hart <me@colin-hart.com>
2017-06-22 09:54:57 -0400

    server configuration

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(logger('dev'));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`listening on 3000`);
+}
